94643b5c8d26f59ee0fcbea798a3d9a4d4fb82f6
omymo <70652236+omymo@users.noreply.github.com>
2021-02-16 14:14:35 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -1,55 +1,88 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import unittest
+from collections import Counter
+
 
 def order(values: list = None) -> list:
     if values is None:
         # TODO: demander les valeurs ici
-        pass
+        values = []
+        while len(values) < 5:
+            values.append(input("saisir une valeur:\n"))
 
-    return []
+    return sorted(values)
 
 
 def anagrams(words: list = None) -> bool:
     if words is None:
         # TODO: demander les mots ici
-        pass
+        words = ()
+        words1 = input("saisir un mot:\n")
+        words2 = input("saisir un autre mot:\n")
+        words = (words1, words2)
+        if sorted(words1) == sorted(words2):
+            return True
 
     return False
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    # return len(set(items)) != len(items)
+    for i in items:
+        if items.count(i) > 1:
+            return True
+        else:
+            return False
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    best_student = dict()
+    for key, value in student_grades.items():
+        average = sum(value) / len(value)
+        if len(best_student) == 0 or list(best_student.values())[0] < average:
+            best_student = {key: average}
+    return best_student
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
+    frequency: {letter: sentence.count(letter) for letter in sentence}
+    sorted_keys = sorted(frequency, reverse=True, key=frequency.__getitem__)
+    for key in sorted_keys:
+        if frequency[key] > 5:
+            print(f"le caractere {key} revient {frequency[key]} fois.")
 
-    return {}
+    return frequency
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    name = input("quel est le nom de votre recette\n")
+    ingredient = input("entrer la liste d'ingredients? separer les ingredients par une, \n").split(",")
+    return {name: ingredient}
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    name = input("quel est le nom de votre recette\n")
+    if name not in ingredients:
+        print("cette recette n'existe pas")
+    else:
+        print(ingredients[name])
 
 
 def main() -> None:
-    print(f"On essaie d'ordonner les valeurs...")
-    order()
+    # print(f"On essaie d'ordonner les valeurs...")
+    #  print(order())
 
-    print(f"On vérifie les anagrammes...")
-    anagrams()
+    # print(f"On vérifie les anagrammes...")
+    # print(anagrams())
 
     my_list = [3, 3, 5, 6, 1, 1]
     print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
@@ -64,7 +97,7 @@ def main() -> None:
     print("On enregistre les recettes...")
     recipes = get_recipes()
 
-    print("On affiche une recette au choix...")
+    # print("On affiche une recette au choix...")
     print_recipe(recipes)
 
 
